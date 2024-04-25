f69d723e9279fc28dc5d02cc3ad0cddded8cf64a
naveenkumar-42 <naveenkumarpoff@gmail.com>
2024-04-25 09:14:18 +0530

    with profile

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,47 @@ app.get('/students', (req, res) => {
         });
 });
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+app.get('/sem', (req, res) => {
+    const sql = 'SELECT * FROM sem';
+    promisePool.query(sql)
+        .then(([rows, fields]) => {
+            return res.json(rows);
+        })
+        .catch((err) => {
+            console.error('Error executing query:', err);
+            return res.json(err); // Send a user-friendly error message
+        });
+});
+
+app.get('/s_skill', (req, res) => {
+    const sql = 'SELECT * FROM s_skill';
+    promisePool.query(sql)
+        .then(([rows, fields]) => {
+            return res.json(rows);
+        })
+        .catch((err) => {
+            console.error('Error executing query:', err);
+            return res.json(err); // Send a user-friendly error message
+        });
+});
+
+app.get('/profile', (req, res) => {
+    const sql = 'SELECT * FROM profile';
+    promisePool.query(sql)
+        .then(([rows, fields]) => {
+            rows.forEach((row) => {
+                row.image = row.image.toString('base64'); // Convert Buffer to base64 string
+            });
+            return res.json(rows);
+        })
+        .catch((err) => {
+            console.error('Error executing query:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        });
+});
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
